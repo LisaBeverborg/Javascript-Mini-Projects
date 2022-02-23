9fa2d2064e0b77715cac25da0933daf8828c77f4
lisa.beverborg <lisa.beverborg@gmail.com>
2022-02-23 16:41:44 +0100

    add save text button

--- CaseConverter/main.js
+++ CaseConverter/main.js
@@ -35,3 +35,24 @@ document.getElementById("sentence-case").addEventListener("click", function sent
     sentenceCase = string.join('. ')
     document.getElementById("textarea").value = sentenceCase;
 });
+
+// save text
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+document.getElementById("save-text-file").addEventListener("click", function(){
+    let text = document.getElementById("textarea").value;
+    let filename = "text.txt";
+
+    download(filename, text);
+}, false);
